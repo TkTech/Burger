967135a6c609c93292a912f7507445884edfa163
Simon Marti <simon@ceilingcat.ch>
2011-07-24 00:19:56 +0200

    Handles method calls and tableswitch instructions
    The current code returns the exact same content for each packet as
    listed on wiki.vg/Protocol
    
    Handling of lookupswitch instructions is still missing

--- burger/toppings/packetinstructions.py
+++ burger/toppings/packetinstructions.py
@@ -21,10 +21,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
-from solum import ClassFile
-
+from solum import ClassFile, JarFile
+from solum.descriptor import method_descriptor
+from solum.classfile.constants import ConstantType
+from solum.bytecode import packed_instruction_size
 from .topping import Topping
-
 
 class PacketInstructionsTopping(Topping):
     """Provides the instructions used to construct a packet"""
@@ -65,10 +66,9 @@ class PacketInstructionsTopping(Topping):
 
     @staticmethod
     def act(aggregate, jar, verbose=False):
-        """Finds all packets and decompiles them"""       
+        """Finds all packets and decompiles them"""
         for packet in aggregate["packets"]["packet"].values():
-            cf = ClassFile(jar["%s.class" % packet["class"]], str_as_buffer=True)
-            packet.update(PacketInstructionsTopping.debunk(cf))
+            packet.update(PacketInstructionsTopping.format(PacketInstructionsTopping.operations(jar, packet["class"])))
 
     @staticmethod
     def find_next(operations, position, operation_search):
@@ -78,37 +78,56 @@ class PacketInstructionsTopping(Topping):
                 return operation
 
     @staticmethod
+    def find_class(cf, instruction):
+        """Finds the class defining the method called in instruction"""
+        return PacketInstructionsTopping.find_constant(cf, instruction.operands[0][1], {ConstantType.FIELD_REF: "class_index", ConstantType.METHOD_REF: "class_index"})
+
+    @staticmethod
     def find_name(cf, instruction):
         """Finds the name of a method called in the suplied instruction"""
-        return PacketInstructionsTopping.find_constant(cf, instruction.operands[0][1], "name_index")
+        return PacketInstructionsTopping.find_constant(cf, instruction.operands[0][1], {ConstantType.NAME_AND_TYPE: "name_index"})
 
     @staticmethod
     def find_descriptor(cf, instruction):
-        """Finds tyoes used in the suplied instruction"""
-        return PacketInstructionsTopping.find_constant(cf, instruction.operands[0][1], "descriptor_index")
+        """Finds types used in an instruction"""
+        return PacketInstructionsTopping.find_constant(cf, instruction.operands[0][1], {ConstantType.NAME_AND_TYPE: "descriptor_index"})
 
     @staticmethod
-    def find_constant(cf, index, index_key):
+    def find_constant(cf, index, custom_follow={}):
         """Walks the constant tree to a name or descriptor"""
         const = PacketInstructionsTopping.constant(cf, index)
-        if const["tag"] == 1:
+        tag = const["tag"]
+        follow = {
+            ConstantType.LONG: "value",
+            ConstantType.CLASS: "name_index",
+            ConstantType.FIELD_REF: "name_and_type_index",
+            ConstantType.METHOD_REF: "name_and_type_index"
+            }
+        follow.update(custom_follow)
+        if tag == ConstantType.UTF8:
             return const["value"]
-        elif const["tag"] == 5:
-            return PacketInstructionsTopping.find_constant(cf, const["value"], index_key)
-        elif const["tag"] == 9 or const["tag"] == 10:
-            return PacketInstructionsTopping.find_constant(cf, const["name_and_type_index"], index_key)
-        elif const["tag"] == 12:
-            return PacketInstructionsTopping.find_constant(cf, const[index_key], index_key)
+        elif follow.has_key(tag):
+            return PacketInstructionsTopping.find_constant(cf, const[follow[tag]], follow)
                 
     @staticmethod
     def constant(cf, index):
-        """Gets a contant by index"""
-        return cf.constants.storage[index]
+        """Gets a constant by index"""
+        return cf.constants[index]
 
     @staticmethod
-    def target(instruction):
+    def target(instruction, index=0):
         """Finds the target of a goto or if instruction"""
-        return instruction.operands[0][1] - 2 + instruction.pos
+        operands = instruction.operands
+        
+        if type(index) != list:
+            index = [index, 1]
+        else:
+            index = [index[0], 1] + index[1:]
+            
+        for i in index:
+            operands = operands[i]
+            
+        return operands + instruction.pos
     
     @staticmethod
     def ordered_operations(operations):
@@ -116,11 +135,16 @@ class PacketInstructionsTopping(Topping):
         return sorted(operations, key=lambda op: op.position)
 
     @staticmethod
-    def debunk(cf):
-        """Does the actual decompiling"""
+    def operations(jar, classname, args=('java.io.DataOutputStream',), methodname=None):
+        """Gets the instructions of the specified method"""
 
         # Find the writing method
-        method = cf.methods.find_one(args=('java.io.DataOutputStream',))
+        cf = jar.open_class(classname)
+
+        if methodname == None:
+            method = cf.methods.find_one(args=args)
+        else:
+            method = cf.methods.find_one(name=methodname, args=args)
 
         # Decode the instructions
         operations = []
@@ -130,27 +154,62 @@ class PacketInstructionsTopping(Topping):
                 type = PacketInstructionsTopping.find_name(cf, instruction)
                 if PacketInstructionsTopping.TYPES.has_key(type):
                     operations.append(Operation(instruction.pos, "write").set("type", PacketInstructionsTopping.TYPES[type]))
-                elif type == "write":
+                else:
                     descriptor = PacketInstructionsTopping.find_descriptor(cf, instruction)
-                    operations.append(Operation(instruction.pos, "write").set("type", "byte[]" if descriptor.find("[B") >= 0 else "byte"))
+                    if type == "write":
+                        operations.append(Operation(instruction.pos, "write").set("type", "byte[]" if descriptor.find("[B") >= 0 else "byte"))
+                    elif descriptor.find("Ljava/io/DataOutputStream") >= 0:
+                        operations += PacketInstructionsTopping.sub_operations(jar, cf, instruction)
             elif instruction.name == "invokestatic":
                 descriptor = PacketInstructionsTopping.find_descriptor(cf, instruction)
-                if descriptor.find("Ljava/lang/String") >= 0 and descriptor.find("Ljava/io/DataOutputStream") >= 0:
-                    operations.append(Operation(instruction.pos, "write").set("type", "string16"))            
+                if descriptor == "(Ljava/lang/String;Ljava/io/DataOutputStream;)V":
+                    operations.append(Operation(instruction.pos, "write").set("type", "string16"))
+                elif descriptor.find("Ljava/io/DataOutputStream") >= 0:
+                    operations += PacketInstructionsTopping.sub_operations(jar, cf, instruction)
+            elif instruction.name == "invokespecial":
+                operations += PacketInstructionsTopping.sub_operations(jar, cf, instruction)
             elif instruction.name.startswith("if"):
                 operations.append(Operation(instruction.pos,"if"))
                 operations.append(Operation(PacketInstructionsTopping.target(instruction),"endif"))
+            elif instruction.name == "tableswitch":
+                operations.append(Operation(instruction.pos, "switch"))
+                low = instruction.operands[0][1][1]
+                for opr in range(1, len(instruction.operands)):
+                    target = PacketInstructionsTopping.target(instruction, opr)
+                    operations.append(Operation(target, "case").set("value", low + opr -  1))
+                operations.append(Operation(PacketInstructionsTopping.target(instruction, [0,0]), "endswitch"))
             elif instruction.name == "goto":
-                target_op = PacketInstructionsTopping.target(instruction)
+                target = PacketInstructionsTopping.target(instruction)
                 endif = PacketInstructionsTopping.find_next(operations, instruction.pos, "endif")
-                if target_op > instruction.pos:
+                case = PacketInstructionsTopping.find_next(operations, instruction.pos, "case")
+                if case != None and target > case.position:
+                    operations.append(Operation(instruction.pos, "break"))  
+                elif target > instruction.pos:
                     endif.operation = "else"
-                    operations.append(Operation(target_op, "endif"))
+                    operations.append(Operation(target, "endif"))
                 else:
                     endif.operation = "endloop"
-                    PacketInstructionsTopping.find_next(operations, target_op, "if").operation = "loop"
+                    PacketInstructionsTopping.find_next(operations, target, "if").operation = "loop"
 
-        # Construct output structure
+        return operations
+
+    @staticmethod
+    def sub_operations(jar, cf, instruction):
+        """Gets the instrcutions of a different class"""
+        invoked_class = PacketInstructionsTopping.find_class(cf, instruction)
+        name = PacketInstructionsTopping.find_name(cf, instruction)
+        args = method_descriptor(PacketInstructionsTopping.find_descriptor(cf, instruction))[0]
+        operations = PacketInstructionsTopping.operations(jar, invoked_class, args, name)
+        position = 0
+        for operation in PacketInstructionsTopping.ordered_operations(operations):
+            position += 0.01
+            operation.position = instruction.pos + (position)
+
+        return operations
+
+    @staticmethod
+    def format(operations):
+        """Constructs output structure"""
         head = []
         stack = [head]
         aggregate = {"instructions": head}
@@ -164,14 +223,21 @@ class PacketInstructionsTopping(Topping):
                         size += PacketInstructionsTopping.SIZES[operation.type]
                     else:
                         size = None
-            if operation.operation in ["endif", "endloop", "else"]:
-                stack.pop()
-                head = stack[-1]
-            if operation.operation in ["if", "loop", "else"]:
-                new_head = []
-                stack.append(new_head)
-                head.append({"operation": operation.operation, "instructions":new_head})
-                head = new_head
+            elif operation.operation == "case":
+                head.append({"operation": "case", "value": operation.value})
+            elif operation.operation in ["break"]:
+                head.append({"operation": operation.operation})
+            else:
+                if operation.operation in ["endif", "endloop", "endswitch", "else"]:
+                    if len(head) == 0:
+                        stack[-2].pop()
+                    stack.pop()
+                    head = stack[-1]
+                if operation.operation in ["if", "loop", "switch", "else"]:
+                    new_head = []
+                    stack.append(new_head)
+                    head.append({"operation": operation.operation, "instructions":new_head})
+                    head = new_head
 
         if size != None:
             aggregate["size"] = size
