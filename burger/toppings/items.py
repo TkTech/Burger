3c304160fd119b17fbeb186d15a1acb968b8a17d
Simon Marti <simon@ceilingcat.ch>
2013-06-27 00:01:38 +0200

    Added icons for items

--- burger/toppings/items.py
+++ burger/toppings/items.py
@@ -67,6 +67,8 @@ class ItemsTopping(Topping):
                     started = True
                     yield instruction
 
+        name_setter = icon_setter = None
+
         for instruction in instructions():
             opcode = instruction.opcode
             if opcode == 0xbb:                      # new
@@ -91,12 +93,19 @@ class ItemsTopping(Topping):
                     item["icon"] = {"x": stack[0], "y": stack[1]}
                 elif len(stack) == 1:
                     if isinstance(stack[0], str):
-                        if "name" in item:
-                            continue
-                        item["name"] = stack[0]
-                        language_key = "%s.name" % stack[0]
-                        if language != None and language_key in language:
-                            item["display_name"] = language[language_key]
+                        const = cf.constants[instruction.operands[0][1]]
+                        method_name = const["name_and_type"]["name"]["value"]
+                        if not name_setter and stack[0] == "shovelIron":
+                            name_setter = method_name
+                        elif not icon_setter and stack[0] == "iron_shovel":
+                            icon_setter = method_name
+                        if method_name == name_setter:
+                            item["name"] = stack[0]
+                            language_key = "%s.name" % stack[0]
+                            if language and language_key in language:
+                                item["display_name"] = language[language_key]
+                        elif method_name == icon_setter:
+                            item["icon"] = stack[0]
                     else:
                         item["stack_size"] = stack[0]
                 stack = []
@@ -105,7 +114,7 @@ class ItemsTopping(Topping):
                 item["id"] = stack[0] + 256
                 stack = []
 
-            elif opcode == 0x12:                    # ldc
+            elif opcode in (0x12, 0x13):            # ldc
                     constant = cf.constants[instruction.operands[0][1]]
                     if constant["tag"] == ConstantType.STRING:
                         stack.append(constant["string"]["value"])
