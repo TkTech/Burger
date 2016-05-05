73b7f3da779c146dc9c63f216d643e62db2bfd34
Tyler Kennedy <tk@tkte.ch>
2016-05-05 15:33:52 -0500

    Fix a completely broken setup.py (not even the right project) Fixes #14.

--- setup.py
+++ setup.py
@@ -1,38 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-"""
-Copyright (c) 2010-2011 Tyler Kennedy <tk@tkte.ch>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name = "Sŏlum",
-    packages = ["solum", "solum.classfile", "solum.context"],
-    version = "0.2.0",
-    description = "A low-level library for JVM class file disassembly.",
-    author = "Tyler Kennedy",
-    author_email = "tk@tkte.ch",
-    url = "http://github.com/TkTech/Solum",
-    keywords = ["java", "disassembly", "disassembler"],
-    classifiers = [
+    name="Burger",
+    packages=find_packages(),
+    version="0.3.0",
+    description="Extract information from Minecraft bytecode.",
+    author="Tyler Kennedy",
+    author_email="tk@tkte.ch",
+    url="http://github.com/mcdevs/Burger",
+    keywords=["java", "minecraft"],
+    install_requires=[
+        'Jawa'
+    ],
+    dependency_links=[
+        'https://github.com/tktech/Jawa/tarball/master#egg=Jawa'
+    ],
+    classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
