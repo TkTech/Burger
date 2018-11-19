1703180abd2a7a86c4273339f12bcfa580e7a075
Pokechu22 <Pokechu022@gmail.com>
2018-11-19 10:40:33 -0800

    Re-add block entity block list support
    In addition, indicate the block entity on the block itself

--- burger/toppings/tileentities.py
+++ burger/toppings/tileentities.py
@@ -20,7 +20,9 @@ class TileEntityTopping(Topping):
 
     DEPENDS = [
         "identify.tileentity.superclass",
-        "packets.classes"
+        "identify.block.superclass",
+        "packets.classes",
+        "blocks"
     ]
 
     @staticmethod
@@ -31,6 +33,7 @@ class TileEntityTopping(Topping):
             return
 
         TileEntityTopping.identify_block_entities(aggregate, classloader, verbose)
+        TileEntityTopping.identify_associated_blocks(aggregate, classloader, verbose)
         TileEntityTopping.identify_network_ids(aggregate, classloader, verbose)
 
     @staticmethod
@@ -78,8 +81,79 @@ class TileEntityTopping(Topping):
                     tmp = {}
 
     @staticmethod
+    def identify_associated_blocks(aggregate, classloader, verbose):
+        te = aggregate["tileentity"]
+        tileentities = te["tileentities"]
+        te_classes = te["classes"]
+
+        blocks = aggregate["blocks"]["block"]
+        # Brewing stands are a fairly simple block entity with a clear hierarchy
+        brewing_stand = blocks["brewing_stand"]
+        cf = classloader[brewing_stand["class"]]
+
+        blockcontainer = cf.super_.name.value
+        cf = classloader[blockcontainer]
+        assert len(cf.interfaces) == 1
+
+        tileentityprovider = cf.interfaces[0].name.value
+        cf = classloader[tileentityprovider]
+        methods = list(cf.methods.find())
+        assert len(methods) == 1
+        create_te_name = methods[0].name.value
+        create_te_desc = methods[0].descriptor.value
+
+        has_be_by_class = {}
+        has_be_by_class[blockcontainer] = True
+        has_be_by_class[aggregate["classes"]["block.superclass"]] = False
+
+        def has_be(cls):
+            if cls in has_be_by_class:
+                return has_be_by_class[cls]
+
+            cf = classloader[cls]
+
+            if has_be(cf.super_.name.value):
+                has_be_by_class[cls] = True
+                return True
+
+            for interface in cf.interfaces:
+                # Final case: if it implements the interface but doesn't directly
+                # extend BlockContainer, it's still a TE
+                if interface.name.value == tileentityprovider:
+                    has_be_by_class[cls] = True
+                    return True
+
+            return False
+
+        blocks_with_be = []
+
+        for block in six.itervalues(blocks):
+            if has_be(block["class"]):
+                blocks_with_be.append(block)
+
+        # OK, we've identified all blocks that have block entities...
+        # now figure out which one each one actually has
+        for block in blocks_with_be:
+            # Find the createNewTileEntity method.
+            # However, it might actually be in a parent class, so loop until it's found
+            cls = block["class"]
+            create_te = None
+            while not create_te:
+                cf = classloader[cls]
+                cls = cf.super_.name.value
+                create_te = cf.methods.find_one(f=lambda m: m.name == create_te_name and m.descriptor == create_te_desc)
+
+            for ins in create_te.code.disassemble():
+                if ins.mnemonic == "new":
+                    const = ins.operands[0]
+                    te_name = te_classes[const.name.value]
+                    block["block_entity"] = te_name
+                    tileentities[te_name]["blocks"].append(block["text_id"])
+                    break
+
+    @staticmethod
     def identify_network_ids(aggregate, classloader, verbose):
-        te = aggregate.setdefault("tileentity", {})
+        te = aggregate["tileentity"]
         tileentities = te["tileentities"]
         te_classes = te["classes"]
 
